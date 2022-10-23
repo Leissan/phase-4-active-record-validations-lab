21b2d6a2f8c382ce9a114139931cd524e076277a
Leissan Khan <leissan.khan@gmail.com>
2022-10-23 11:59:09 -0400

    commit tree 02651e7dac6f0cdb709fe5b56fef0dd334c539dc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validate :must_have_clickbait
+
+    def must_have_clickbait
+        if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "Needs more clickbait")
+        end
+        
+    end
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
 end
